173bc13a20a44ec5378aaec619be4b8709ec78fd
zhangchao@vv.cn <zhangchao@vv.cn>
2024-09-26 14:31:43 +0800

    feat: 变量与常量声明

--- test01/go.mod
+++ test01/go.mod
@@ -1,3 +1,3 @@
-module github.com/yourusername/go-test
+module go-test
 
 go 1.23.1
---
+++ test01/test2.go
@@ -0,0 +1,26 @@
+package main
+
+var (
+	a1 = 123
+	a2 = "hello"
+	Ds = "hello world"
+)
+
+const (
+	A1 = "1111"
+	b1 = 222
+)
+
+func test2() {
+	// 变量声明
+	var b int = 456
+	var c string = "111"
+
+	println("test2")
+
+	println(b)
+	println(c)
+
+	println("%s", a2)
+
+}
